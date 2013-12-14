812c502a4058bc6e02670328e0ba418080349c97
Nestor Albelo <alu0100598322@ull.edu.es>
2013-12-14 19:30:38 +0000

    Correccion de errores

--- lib/MatricesDSL.rb
+++ lib/MatricesDSL.rb
@@ -4,8 +4,7 @@ require "Matrices_dispersas.rb"
 class MatricesDSL
   
   def initialize(op, &block)
-      @operand1
-      @operand2
+      @operand = []
       @tipo
       @operacion=op
       @resultado
@@ -13,48 +12,14 @@ class MatricesDSL
       instance_eval &block
   end
   
-  def to_s
-      output = "\n#{@operacion} de #{@tipo}"
-      output << "\n#{'=' * output.size}\n\n"
-      output << "Operacion: \n"
-      if (@operand2)
-          output << "\t#{@operand1.to_s} #{@operacion} #{@operand2.to_s}\n"
-      else
-          output << "\t#{@operacion} #{@operand1.to_s}\n"
-      end
-      output << "Resultado:\n"
-      
-      output << "\t#{@resultado}"  
-      output
-  end
-  
   def operand(*arg)
-      if arg.size == 2 && @tipo=='Densas'
-	@operand1=MatricesDen.new(arg[0],arg[1])
-      else
-	 if arg.size == 3 && @tipo=='Dispersas'
-	    @operand1 = MatricesDis.new(arg[0],arg[1],arg[2])
-	 else
-	   if arg.size == 4 && @tipo=='Densas'
-	      @operand1=MatricesDen.new(arg[0],arg[1])
-	      @operand2=MatricesDen.new(arg[2],arg[3])
-	   else
-	     if arg.size == 6 && @tipo=='Dispersas'
-		@operand1=MatricesDis.new(arg[0],arg[1],arg[2])
-		@operand2=MatricesDis.new(arg[3],arg[4],arg[5])
-	     else
-		if @tipo == 'DensasDispersas'
-		  @operand1=MatricesDen.new(arg[0],arg[1])
-		  @operand2=MatricesDis.new(arg[2],arg[3],arg[4])
-		else
-		  @operand1=MatricesDis.new(arg[0],arg[1],arg[2])
-		  @operand2=MatricesDen.new(arg[3],arg[4])
-		end
-	     end
-	   end
-	 end
-      end    
-      operation
+  	if arg.size == 2
+  		@operand << MatricesDen.new(arg[0],arg[1])
+  	elsif arg.size == 3
+  		@operand << MatricesDis.new(arg[0],arg[1],arg[2])
+  	else 
+  		puts "Error al introducir la matriz"
+  	end
   end
 
   def option(text)
@@ -67,29 +32,33 @@ class MatricesDSL
   end
   
   def operation
-  if @operacion == "Suma"
-     @resultado=@operand1 + @operand2
-  else
-      if @operacion == 'Resta'
-        @resultado=@operand1 - @operand2
-      else
-          if @operacion == 'Multiplicacion'
-            @resultado=@operand1 * @operand2
-          else
-            if @operacion == 'Opuesto'
-              @resultado=-@operand1
-            else
-              if @operacion == 'Max'
-                @resultado=@operand1.max
-              else
-                if @operacion == 'Min'
-                  @resultado=@operand1.min
-                end
-              end
-            end
-          end
-        end
-      end
+  	(@operand.size).times do |i|
+  		if @operacion == 'Suma' && (i<@operand.size-1)
+    		@resultado=@operand[i] + @operand[i+1]
+  		elsif @operacion == 'Resta' && (i<@operand.size-1)
+        	@resultado=@operand[i] - @operand[i+1]
+  		elsif @operacion == 'Multiplicacion' && (i<@operand.size-1)
+        	@resultado=@operand[i] * @operand[i+1]
+ 		elsif @operacion == 'Opuesto'
+        	@resultado=-@operand[i]
+    	elsif @operacion == 'Max'
+        	@resultado=@operand[i].max
+   	 	elsif @operacion == 'Min'
+        	@resultado=@operand[i].min
+    	end
+
+      	if (i<@operand.size-1 && (@operacion == "Suma" || @operacion == "Resta" || @operacion == 'Multiplicacion'))
+      	  puts "\nOperacion: \n"
+          puts "\t#{@operand[i].to_s} #{@operacion} #{@operand[i+1].to_s}\n"
+          puts "Resultado:\n"
+      	  puts "\t#{@resultado}"
+      	elsif (@operacion == "Opuesto" || @operacion == "Max" || @operacion == 'Min')
+      	  puts "\nOperacion: \n"
+          puts "\t#{@operacion} #{@operand[i].to_s}\n"
+          puts "Resultado:\n"
+      	  puts "\t#{@resultado}"
+      	end  
+	end
   end
 
 end  
--- lib/Prueba.rb
+++ lib/Prueba.rb
@@ -2,126 +2,137 @@ require "MatricesDSL.rb"
 
 #Densas
 #----------------------------------------------------------------------
+puts "DENSAS"
+puts "#{'*' * 60}"
 sumaDen = MatricesDSL.new("Suma") do
     option "Densas"
-    operand 2,[1,2,3,4], 2, [1,2,3,4]
+    operand 2, [1,2,3,4]
+    operand 2, [1,2,3,4]
+    operation
 end
 
 restaDen = MatricesDSL.new("Resta") do
     option "Densas"
-    operand 2,[1,2,3,4], 2, [1,2,3,4]  
+    operand 2, [1,2,3,4]
+    operand 2, [1,2,3,4]  
+    operation
 end
 
 mutiDen = MatricesDSL.new("Multiplicacion") do
     option "Densas"
-    operand 2,[1,2,3,4], 2, [1,2,3,4] 
+    operand 2, [1,2,3,4]
+    operand 2, [1,2,3,4] 
+    operation
 end
 
 opuestoDen = MatricesDSL.new("Opuesto") do
     option "Densas" 
-    operand 2,[1,2,3,4]  
+    operand 2, [1,2,3,4] 
+    operation 
 end
 
 maxDen = MatricesDSL.new("Max") do
     option "Densas" 
-    operand 2,[1,2,3,4]  
+    operand 2, [1,2,3,4]  
+    operation
 end
 
 minDen = MatricesDSL.new("Min") do
     option "Densas" 
-    operand 2,[1,2,3,4]  
+    operand 2, [1,2,3,4]  
+    operation
 end
 #Dispersa
 #----------------------------------------------------------------------
+puts "\n\nDISPERSAS"
+puts "#{'*' * 60}"
 sumaDis = MatricesDSL.new("Suma") do
     option "Dispersas"
-    operand 2,[1,2],[0,1],2,[1,2],[0,1]
+    operand 2, [1,2],[0,1]
+    operand 2, [1,2],[0,1]
+    operation
 end
 
 restaDis = MatricesDSL.new("Resta") do
     option "Dispersas"
-    operand  2, [1,2], [0,1],2, [1,2], [0,1]
+    operand 2, [1,2], [0,1]
+    operand 2, [1,2], [0,1]
+    operation
 end
 
 mutiDis = MatricesDSL.new("Multiplicacion") do
     option "Dispersas"
-    operand  2, [1,2], [0,1],2, [1,2], [0,1]
+    operand 2, [1,2], [0,1]
+    operand 2, [1,2], [0,1]
+    operation
 end
 
 opuestoDis = MatricesDSL.new("Opuesto") do
     option "Dispersas" 
     operand  2, [1,2], [0,1]
+    operation
 end
 
 maxDis = MatricesDSL.new("Max") do
     option "Dispersas" 
     operand  2, [1,2], [0,1]  
+    operation
 end
 
 minDis = MatricesDSL.new("Min") do
     option "Dispersas" 
     operand  2, [1,2], [0,1]
+    operation
 end
 
 #Densa Dispersa
 #----------------------------------------------------------------------
+puts "\n\nDENSAS DISPERSAS"
+puts "#{'*' * 60}"
 sumaDenDis = MatricesDSL.new("Suma") do
     option "DensasDispersas"
-    operand 2,[1,2,3,4], 2, [1,2], [0,1]
+    operand 2, [1,2,3,4]
+    operand 2, [1,2], [0,1]
+    operation
 end
 
 restaDenDis = MatricesDSL.new("Resta") do
     option "DensasDispersas"
-    operand 2,[1,2,3,4], 2, [1,2], [0,1]
+    operand 2, [1,2,3,4]
+    operand 2, [1,2], [0,1]
+    operation
 end
 
 mutiDenDis = MatricesDSL.new("Multiplicacion") do
     option "DensasDispersas"
-    operand 2,[1,2,3,4], 2, [1,2], [0,1] 
+    operand 2, [1,2,3,4]
+    operand 2, [1,2], [0,1] 
+    operation
 end
 
 #Dispersa Densa
 #----------------------------------------------------------------------
+puts "\n\nDISPERSAS DENSAS"
+puts "#{'*' * 60}"
 sumaDisDen = MatricesDSL.new("Suma") do
     option "DispersasDensas"
-    operand 2, [1,2], [0,1], 2,[1,2,3,4]
+    operand 2, [1,2], [0,1]
+    operand 2, [1,2,3,4]
+    operation
 end
 
 restaDisDen = MatricesDSL.new("Resta") do
     option "DispersasDensas"
-    operand 2, [1,2], [0,1], 2,[1,2,3,4]
+    operand 2, [1,2], [0,1]
+    operand 2, [1,2,3,4]
+    operation
 end
 
 mutiDisDen = MatricesDSL.new("Multiplicacion") do
     option "DispersasDensas"
-    operand 2, [1,2], [0,1], 2,[1,2,3,4] 
+    operand 2, [1,2], [0,1]
+    operand 2, [1,2,3,4] 
+    operation
 end
 
 
-
-puts "DENSAS"
-puts "#{'*' * 60}"
-puts sumaDen
-puts restaDen
-puts mutiDen
-puts opuestoDen
-puts minDen
-puts maxDen
-puts "\n\nDISPERSAS"
-puts "#{'*' * 60}"	
-puts sumaDis
-puts restaDis
-puts mutiDis
-puts opuestoDis
-puts minDis
-puts maxDis
-puts "\n\nDENSAS DISPERSAS"
-puts "#{'*' * 60}"
-puts sumaDenDis
-puts restaDenDis
-puts mutiDenDis
-puts "\n\nDISPERSAS DENSAS"
-puts "#{'*' * 60}"
-puts sumaDisDen
-puts restaDisDen 
-puts mutiDisDen
